5a75292c1c340c6d2b3c8586f1ea370fb9d4f14c
nour bouattour <nourbouattour173@gmail.com>
2026-05-10 16:51:22 +0100

    modif

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ $erreur = "";
 require_once __DIR__ . '/includes/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email    = trim($_POST['email']);
+    $email    = trim($_POST['email']);#trim:enléve les espaces
     $password = $_POST['password'];
 
     if (empty($email) || empty($password)) {
